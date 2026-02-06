44c407cbb328fb6a164c2c8e47d0ca565c9e32b5
Joserraa05 <juke15yt@gmail.com>
2026-02-06 13:08:48 +0100

    Añadidas la estructura de carpeta, navbar y footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../components/layout/navbar/navbar';
+import { Footer } from '../components/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
