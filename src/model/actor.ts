90bf1a7a1f970bbe93da1e2dff29da1da1c22d7f
José Manuel Jiménez Rodríguez <josemajimenezrodriguez8@gmail.com>
2026-02-06 18:16:34 +0100

    Creación de los modelos de Actor y Ayuntamiento

---
+++ src/model/actor.ts
@@ -0,0 +1,11 @@
+export class Actor {
+    id!: number; 
+    nombre!: string;
+    foto?: string;
+    correo!: string;
+    telefono?: string;
+    direccion?: string;
+    username!: string;
+    rol!: string;
+    baneado!: boolean;
+}
