229ca16be5d6c874eef251cd7163894a405f6507
José Manuel Jiménez Rodríguez <josemajimenezrodriguez8@gmail.com>
2026-02-07 13:18:08 +0100

    Añadido el componente de table-ayuntamiento e includo en navbar y app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TableSolicitudLicencia } from '../components/solicitud-licencia/table-solicitud-licencia/table-solicitud-licencia';
+import { TableAyuntamiento } from '../components/ayuntamiento/table-ayuntamiento/table-ayuntamiento';
 
 export const routes: Routes = [
-  { path: 'solicitudes', component: TableSolicitudLicencia }
+  { path: 'solicitudes', component: TableSolicitudLicencia },
+  { path: 'ayuntamientos', component: TableAyuntamiento }
 ];
---
+++ src/components/ayuntamiento/table-ayuntamiento/table-ayuntamiento.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { Ayuntamiento } from '../../../model/ayuntamiento';
+import { AyuntamientoService } from '../../../service/ayuntamiento-service';
+
+@Component({
+  selector: 'app-table-ayuntamiento',
+  standalone: true,
+  imports: [CommonModule], 
+  templateUrl: './table-ayuntamiento.html', 
+  styleUrl: './table-ayuntamiento.css'      
+})
+
+export class TableAyuntamiento implements OnInit {
+  
+  ayuntamientos: Ayuntamiento[] = [];
+  cdr = inject(ChangeDetectorRef);
+
+  constructor(private ayuntamientoService: AyuntamientoService) {
+
+  }
+
+  ngOnInit(): void {
+    this.cargarAyuntamientos();
+  }
+
+  cargarAyuntamientos(): void {
+    this.ayuntamientoService.getAyuntamientos().subscribe({
+      next: (data) => {
+        this.ayuntamientos = data;
+        this.cdr.detectChanges();
+      },
+      error(err) {
+        console.error("Error al cargar los ayuntamientos: ", err);
+      },
+    })
+  }
+  }
