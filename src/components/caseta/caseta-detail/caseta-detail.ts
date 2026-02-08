d8f3110de35536825b9d76143de22fdabb89f395
Juan José Gamero López <jgamlop2101@g.educaand.es>
2026-02-08 23:19:22 +0100

    Vista detallada de caseta

---
+++ src/components/caseta/caseta-detail/caseta-detail.ts
@@ -0,0 +1,58 @@
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CasetaService } from '../../../service/caseta-service';
+import { Caseta, EstadoCaseta } from '../../../model/caseta';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-caseta-detail',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './caseta-detail.html',
+  styleUrl: './caseta-detail.css',
+})
+export class CasetaDetail implements OnInit {
+  id!: string;
+  caseta!: Caseta;
+  cdr = inject(ChangeDetectorRef);
+
+  constructor(
+    private route: ActivatedRoute,
+    private casetaService: CasetaService
+  ) {
+    this.id = this.route.snapshot.paramMap.get('id')!;
+  }
+
+  ngOnInit(): void {
+    if (this.id != null) {
+      this.cargarCaseta(Number.parseInt(this.id));
+    }
+  }
+
+  cargarCaseta(id: number) {
+    this.casetaService.getCaseta(id).subscribe({
+      next: (data: Caseta) => {
+        this.caseta = data;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Error al cargar la caseta:', err);
+      }
+    });
+  }
+
+  getBadgeClass(estado?: EstadoCaseta): string {
+    switch (estado) {
+      case EstadoCaseta.DISPONIBLE:
+        return 'bg-success';
+      case EstadoCaseta.OCUPADA:
+        return 'bg-danger';
+      case EstadoCaseta.MANTENIMIENTO:
+        return 'bg-warning';
+      case EstadoCaseta.RESERVADA:
+        return 'bg-info';
+      default:
+        return 'bg-secondary';
+    }
+  }
+}
